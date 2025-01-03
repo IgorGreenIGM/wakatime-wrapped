4b53013d96b946ff72da325f668cf779acba83a8
IgorGreenIGM <107557907+IgorGreenIGM@users.noreply.github.com>
2025-01-03 16:30:30 +0100

    fixed vite.config.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // src/App.jsx
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Navbar from './components/NavBar/Navbar';
 import MainPage from './pages/MainPage';
